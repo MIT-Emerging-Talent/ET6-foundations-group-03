757d7807c77e0ab4f1345116828fab42dc7ca14b
asagulenko <a.sagulenko@GMAIL.COM>
2025-01-07 21:12:28 -0500

    Formatting function file.2

--- solutions/only_even_numbers.py
+++ solutions/only_even_numbers.py
@@ -13,34 +13,28 @@ Returns:
     list: A list of even numbers pulled from the text.
 
 Examples:
-    >>> only_even_numbers('')
+    >>> only_even_numbers("")
     []
-    >>> only_even_numbers('2+2-4*7=-24')
-    ['2', '2', '4', '24']
-    >>> only_even_numbers('Today is January 2nd 2025')
-    ['2']
+    >>> only_even_numbers("2+2-4*7=-24")
+    ["2", "2", "4", "24"]
+    >>> only_even_numbers("Today is January 2nd 2025")
+    ["2"]
 
 Raises:
     ValueError: If the input is not a string.
 """
 
-import re  # Importing Regular Expressions package
+import re
 
 
 def only_even_numbers(text: str) -> list[str]:
+    
     """Extract even numbers from a given text string."""
     
-    # Raises ValueError if the argument is not a string
     if not isinstance(text, str):
         raise ValueError("Input must be a string")
 
-    # Extract all numbers from the string
     all_numbers = re.findall(r"\d+", text)
-
-    # Filter out only even numbers
-    even_numbers=[]
-    for number in all_numbers:
-        if int(number) % 2 == 0:
-            even_numbers.append(number)
+    even_numbers = [number for number in all_numbers if int(number) % 2 == 0]
 
     return even_numbers
